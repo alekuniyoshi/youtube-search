9de527fb1c0b3ae9a3797e1b905afd26443780c0
Alejandro Kuniyoshi <alejandro.kuniyoshi@gmail.com>
2021-12-18 16:39:24 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchInputComponent } from './search/components/search-input/search-input.component';
+import { SearchListComponent } from './search/components/search-list/search-list.component';
+import { SearchContainerComponent } from './search/container/search-container/search-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchInputComponent,
+    SearchListComponent,
+    SearchContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/search/components/search-list/search-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Video } from 'src/app/shared/models/search.interface';
+
+@Component({
+  selector: 'app-search-list',
+  templateUrl: './search-list.component.html',
+  styleUrls: ['./search-list.component.css']
+})
+export class SearchListComponent implements OnInit {
+
+  @Input()
+  videos: Video[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
