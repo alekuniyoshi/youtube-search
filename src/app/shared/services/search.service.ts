d0d7cc581e774b4f56b1931c01bd78ef3b906884
Alejandro Kuniyoshi <alejandro.kuniyoshi@gmail.com>
2021-12-19 20:55:48 -0300

    search inputs

--- src/app/shared/services/search.service.ts
+++ src/app/shared/services/search.service.ts
@@ -12,7 +12,7 @@ export class SearchService {
   constructor(private http: HttpClient) { }
 
   getVideos(query: string): Observable<any> {
-    const url = `${this.API_URL}?q=${query}&key=${this.API_TOKEN}&part=snippet&type=video&maxResults=10`;
+    const url = `${this.API_URL}?q=${query}&key=${this.API_TOKEN}&part=snippet&type=video&maxResults=50`;
     return this.http.get(url)
       .pipe(
         map((response: any) => response.items));
